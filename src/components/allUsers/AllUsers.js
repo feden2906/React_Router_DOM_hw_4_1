94ee72218fb2b35cde6a7ac2a127b9935fbefbf0
freefireman <beluh.c.s@gmail.com>
2020-12-10 19:23:28 +0200

    little fix))

--- src/components/allUsers/AllUsers.js
+++ src/components/allUsers/AllUsers.js
@@ -1,23 +1,33 @@
 import React, {Component} from 'react';
 import './AllUsers.css'
 import User from "../User/User";
-import {Route, Switch, withRouter} from "react-router-dom";
+import {Route, withRouter} from "react-router-dom";
 import FullUser from "../fullUser/FullUser";
+import doFetch from  './../services/doFetch'
 
 class AllUsers extends Component {
+    state = {users: false}
+    componentDidMount() {
+        const {match:{url}} = this.props
 
-  render() {
-    const {users, match:{url}} = this.props
+        doFetch(url).then(users => {
+            this.setState({users})
+        })
+    }
+
+    render() {
+    const {match:{url}} = this.props
+        const {users} = this.state;
     return (
         <div>
-          {users.map(value => <User user={value} key={value.id}/>)}
+          {users && users.map(value => <User user={value} key={value.id}/>)}
           <hr/>
-          <Switch>
+
             <Route path={url+ '/:id'} render={(props => {
               const {match:{params:{id}}} = props;
               return <FullUser id={id} key={id}/>
             })}/>
-          </Switch>
+
         </div>
     );
   }
